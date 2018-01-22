6b9de1a33c915f047b0b15eb380c535d9cdae40a
Svetlana Tchistiakova <stchistiakova@gmail.com>
2018-01-22 23:48:02 +0100

    play around with training the models

--- w2v.py
+++ w2v.py
@@ -4,11 +4,20 @@ import w2vconfig
 import gensent
 
 
-#just some data to play around with for now
-directory = os.path.join(os.path.join(w2vconfig.data_dir, 'en'), 'train')
+nl_direc = os.path.join(w2vconfig.data_dir, 'nl')
+ru_direc = os.path.join(w2vconfig.data_dir, 'ru')
 
-sentences = gensent.SentenceGenerator()
-sentences.read_directory(directory)
-model = gensim.models.Word2Vec(sentences, min_count=1)
-words = list(model.wv.vocab)
-print(model['to'])
+nl_sents = gensent.SentenceGenerator(language='dutch')
+ru_sents = gensent.SentenceGenerator(language='russian')
+
+nl_sents.read_directory(nl_direc)
+ru_sents.read_directory(ru_direc)
+
+nl_model = gensim.models.Word2Vec(nl_sents, **w2vconfig.gensim_config)
+ru_model = gensim.models.Word2Vec(ru_sents, **w2vconfig.gensim_config)
+
+nl_vectors = nl_model.wv
+ru_vectors = ru_model.wv
+
+nl_vectors.save('nl_vectors')
+ru_vectors.save('ru_vectors')
