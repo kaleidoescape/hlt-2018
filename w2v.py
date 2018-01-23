b597e70c1080d6359a07ab5ffc13a0092b0fcbcd
Svetlana Tchistiakova <stchistiakova@gmail.com>
2018-01-23 07:54:02 +0100

    playing around with gensim

--- w2v.py
+++ w2v.py
@@ -5,19 +5,15 @@ import gensent
 
 
 nl_direc = os.path.join(w2vconfig.data_dir, 'nl')
-ru_direc = os.path.join(w2vconfig.data_dir, 'ru')
-
-nl_sents = gensent.SentenceGenerator(language='dutch')
-ru_sents = gensent.SentenceGenerator(language='russian')
-
+nl_sents = gensent.SentenceGenerator(language='dutch', maxsents=100)
 nl_sents.read_directory(nl_direc)
-ru_sents.read_directory(ru_direc)
-
 nl_model = gensim.models.Word2Vec(nl_sents, **w2vconfig.gensim_config)
-ru_model = gensim.models.Word2Vec(ru_sents, **w2vconfig.gensim_config)
-
 nl_vectors = nl_model.wv
-ru_vectors = ru_model.wv
-
 nl_vectors.save('nl_vectors')
+
+ru_direc = os.path.join(w2vconfig.data_dir, 'ru')
+ru_sents = gensent.SentenceGenerator(language='russian', maxsents=100)
+ru_sents.read_directory(ru_direc)
+ru_model = gensim.models.Word2Vec(ru_sents, **w2vconfig.gensim_config)
+ru_vectors = ru_model.wv
 ru_vectors.save('ru_vectors')
