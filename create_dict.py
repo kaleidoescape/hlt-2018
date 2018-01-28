226e1a35281286ec9b106e8187e6f75ce334f5b7
Svetlana Tchistiakova <stchistiakova@gmail.com>
2018-01-28 13:04:40 +0100

    add parse args to create_dict

--- create_dict.py
+++ create_dict.py
@@ -1,5 +1,39 @@
+import os
+import sys
 from pymystem3 import Mystem
 import nl_lemmatizer
+import w2vconfig
+
+class AlterParser(argparse.ArgumentParser):
+    """Change behaviour of ArgumentParser.error() to print help and exit."""
+    def error(self, msg):
+        sys.stderr.write('error: %s\n' % msg)
+        self.print_help()
+        sys.exit(2)
+
+def parse_args():
+    """Parse command line arguments."""
+    parser = AlterParser(prog='create_dict.py', 
+            description='Create a Dutch-Russian dictionary from English-Dutch and English-Russian ones.')
+    parser.add_argument('--en-nl',
+        help='file path to English-Dutch dictionary')
+    parser.add_argument('--en-ru',
+        help='file path to English-Russian dictionary')
+    parser.add_argument('--nl-ru',
+        help='where to create the new Dutch-Russian dictionary')
+    args = parser.parse_args()
+
+    if not args.en-nl:
+        args.en-nl = 'en-nl.txt'
+    if not args.en-ru:
+        args.en-ru = 'en-ru.txt'
+    if not args.nl-ru:
+        args.nl-ru = w2config.nl_ru_dict
+
+    if os.path.exists(args.nl-ru):
+        print('Dutch-Russian dictionary already exists: {}'.format(args.nl-ru))
+        sys.exit(0)
+    return args
 
 def load_dict(src_tar_fp):
     d = {}
@@ -32,9 +66,11 @@ def merge_dicts(dict_a, dict_b):
 
 
 if __name__ == '__main__':
+    args = parse_args()
+
     r = Mystem()
-    en_nl_fp = 'en-nl.txt'
-    en_ru_fp = 'en-ru.txt'
+    en_nl_fp = args.en-nl
+    en_ru_fp = args.en-ru
     en_nl = load_dict(en_nl_fp)
     en_ru = load_dict(en_ru_fp)
     merged = merge_dicts(en_nl, en_ru)
@@ -50,6 +86,6 @@ if __name__ == '__main__':
                 for ru_word in ru:
                     ru_word = r.lemmatize(ru_word)[0]
                     triplets.add((nl_word, ru_word, en_word))
-    with open('merged.txt', 'w', encoding='utf-8') as outfp:
+    with open(args.nl-ru, 'w', encoding='utf-8') as outfp:
         for triplet in triplets:
             outfp.write('{} {} {}\n'.format(triplet[2], triplet[0], triplet[1])) 
